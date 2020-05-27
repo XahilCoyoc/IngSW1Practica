8f26f98b1e5df830fbca3755242a82f86d9e25a1
Xahil <Xahil coyocitzel@gmail.com>
2020-05-26 23:17:39 -0500

    Proyecto Final

---
+++ src/test/java/com/fca/calidad/funcionales/Editar.java
@@ -0,0 +1,81 @@
+package com.fca.calidad.funcionales;
+
+import java.util.regex.Pattern;
+import java.util.concurrent.TimeUnit;
+import org.junit.*;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class Editar {
+  private WebDriver driver;
+  private String baseUrl;
+  private boolean acceptNextAlert = true;
+  private StringBuffer verificationErrors = new StringBuffer();
+
+  @Before
+  public void setUp() throws Exception {
+	 System.setProperty("webdriver.chrome.driver", "/Users/usuario/Downloads/FCA/ChromeDriver/chromedriver.exe");
+    driver = new ChromeDriver();
+    baseUrl = "https://mern-crud.herokuapp.com/";
+    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+  }
+
+  @Test
+  public void testEditar() throws Exception {
+    driver.get("https://mern-crud.herokuapp.com/");
+    driver.findElement(By.xpath("//div[@id='root']/div/div[2]/table/tbody/tr/td[5]/button")).click();
+    driver.findElement(By.name("age")).click();
+    driver.findElement(By.name("age")).clear();
+    driver.findElement(By.name("age")).sendKeys("35");
+    driver.findElement(By.xpath("//form/button")).click();
+    // Warning: assertTextPresent may require manual changes
+    assertTrue(driver.findElement(By.cssSelector("BODY")).getText().matches("^[\\s\\S]*$"));
+    driver.findElement(By.xpath("//i")).click();
+  }
+
+  @After
+  public void tearDown() throws Exception {
+    driver.quit();
+    String verificationErrorString = verificationErrors.toString();
+    if (!"".equals(verificationErrorString)) {
+      fail(verificationErrorString);
+    }
+  }
+
+  private boolean isElementPresent(By by) {
+    try {
+      driver.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private boolean isAlertPresent() {
+    try {
+      driver.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+
+  private String closeAlertAndGetItsText() {
+    try {
+      Alert alert = driver.switchTo().alert();
+      String alertText = alert.getText();
+      if (acceptNextAlert) {
+        alert.accept();
+      } else {
+        alert.dismiss();
+      }
+      return alertText;
+    } finally {
+      acceptNextAlert = true;
+    }
+  }
+}
+
